1202661e7b287b0553d168025c5e88441c3e29e3
Addison Dalton <zarch14@gmail.com>
2018-02-13 16:07:51 -0500

    added basic functionality to populate database with data from a spreadsheet, then show that data in a GridView

--- ExcelToSQLServer/Default.aspx.cs
+++ ExcelToSQLServer/Default.aspx.cs
@@ -14,17 +14,18 @@ namespace ExcelToSQLServer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void UpdateData_Click(object sender, EventArgs e)
         {
             DataTable excelRecords = new DataTable();
-
+            string dbConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Owner\\source\\repos\\ExcelToSQLServer\\ExcelToSQLServer\\App_Data\\SqlServerDataSource.mdf;Integrated Security=True;MultipleActiveResultSets=True";
             GetExcelDatasource(excelRecords);
-            PopulateDatabase(excelRecords);
+            PopulateDatabase(excelRecords, dbConnectionString);
+            DisplayGridView(dbConnectionString);
         }
 
+        //gets the data from the excel spreadsheet and holds it within excelRecords.
         private void GetExcelDatasource(DataTable dataTable)
         {
             string fileName = "dataSource.xlsx";
@@ -43,19 +44,56 @@ namespace ExcelToSQLServer
             excelCmd.CommandText = "SELECT * FROM [" + getExcelSheet + "]";
             dataAdapter.SelectCommand = excelCmd;
             dataAdapter.Fill(dataTable);
-            dataGrid.DataSource = dataTable;
-            dataGrid.DataBind();
         }
 
-        private void PopulateDatabase(DataTable dataTable)
+        //populates the database with data from the passed dataTable, which holds data from the excel spreadsheet.
+        private void PopulateDatabase(DataTable dataTable, string connectionString)
         {
-            String ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Owner\\source\\repos\\ExcelToSQLServer\\ExcelToSQLServer\\App_Data\\SqlServerDataSource.mdf;Integrated Security=True";
-            SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(ConnectionString);
-            sqlBulkCopy.ColumnMappings.Add("ID", "Id");
-            sqlBulkCopy.ColumnMappings.Add("First Name", "FirstName");
-            sqlBulkCopy.ColumnMappings.Add("Last Name", "LastName");
-            sqlBulkCopy.DestinationTableName = "Excel_Table";
-            sqlBulkCopy.WriteToServer(dataTable);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                //remove all records from staging table to be sure it is empty before populating it
+                SqlCommand deleteStagingRecords = new SqlCommand("DELETE FROM Staging_Table", connection);
+                deleteStagingRecords.BeginExecuteNonQuery();
+
+                //
+                SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(connectionString);
+                sqlBulkCopy.ColumnMappings.Add("ID", "Id");
+                sqlBulkCopy.ColumnMappings.Add("First Name", "FirstName");
+                sqlBulkCopy.ColumnMappings.Add("Last Name", "LastName");
+                sqlBulkCopy.DestinationTableName = "Staging_Table";
+
+                //catch for errors that may occur when writing to the staging table.
+                try
+                {
+                    sqlBulkCopy.WriteToServer(dataTable);
+                }
+                catch (Exception ex)
+                {
+                    resultLabel.Text = ex.ToString();
+                    //resultLabel.Text = "There was a problem writing to the database";
+                }
+
+                //merge the staging table into the primary Excel_Table
+                SqlCommand mergeCommand = new SqlCommand("MergeTestProc", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                mergeCommand.BeginExecuteNonQuery();
+            }
+        }
+
+        //gets the data from database and displays it in the gridview.
+        private void DisplayGridView(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Excel_Table", connection);
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+                dataGrid.DataSource = dataReader;
+                dataGrid.DataBind();
+            }
         }
     }
 }
