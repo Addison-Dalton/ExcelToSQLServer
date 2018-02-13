4824ff5744ada22c6e4ec47ebd3e96a44204cd6e
Addison Dalton <zarch14@gmail.com>
2018-02-13 11:58:40 -0500

    added functionality to extract data from excel file and display within gridview

--- ExcelToSQLServer/Default.aspx
+++ ExcelToSQLServer/Default.aspx
@@ -4,11 +4,22 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Excel to SQL Server</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
+            <div>
+                <asp:GridView ID="dataGrid" runat="server"></asp:GridView>
+            </div>
+            <br />
+            <div>
+                <asp:Button ID="dataButton" Text="Update" OnClick="UpdateData_Click" runat="server" />
+            </div>
+            <br />
+            <div>
+                <asp:Label ID="resultLabel" Text="" runat="server"></asp:Label>
+            </div>
         </div>
     </form>
 </body>
--- ExcelToSQLServer/Default.aspx.cs
+++ ExcelToSQLServer/Default.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +13,35 @@ namespace ExcelToSQLServer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void UpdateData_Click(object sender, EventArgs e)
+        {
+            GetExcelDatasource();
+        }
+
+        private void GetExcelDatasource()
+        {
+            string fileName = "dataSource.xlsx";
+            string fileLocation = Server.MapPath("~/SpreadSheets/" + fileName);
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+                fileLocation + ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
+
+            OleDbConnection excelConnection = new OleDbConnection(connectionString);
+            OleDbCommand excelCmd = new OleDbCommand();
+            excelCmd.CommandType = System.Data.CommandType.Text;
+            excelCmd.Connection = excelConnection;
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(excelCmd);
+            DataTable excelRecords = new DataTable();
+            excelConnection.Open();
+            DataTable excelSheet = excelConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string getExcelSheet = excelSheet.Rows[0]["Table_Name"].ToString();
+            excelCmd.CommandText = "SELECT * FROM [" + getExcelSheet + "]";
+            dataAdapter.SelectCommand = excelCmd;
+            dataAdapter.Fill(excelRecords);
+            dataGrid.DataSource = excelRecords;
+            dataGrid.DataBind();
 
         }
     }
